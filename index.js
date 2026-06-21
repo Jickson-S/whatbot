d8c632c846a151faa5f57f6aa47a157c5131cb08
Jickson-S <geojickson@gmail.com>
2026-06-22 00:12:26 +0530

    k

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
         args: [
