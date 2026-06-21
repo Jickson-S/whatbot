f52a7c28ba54ffbc337df36d76111b6cb05cab80
Jickson-S <geojickson@gmail.com>
2026-06-21 23:11:30 +0530

    b

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage'
+        ]
     }
 });
 
