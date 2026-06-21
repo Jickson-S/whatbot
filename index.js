97933987f02a9dd9b36ea78a33d452de177f38d9
Jickson-S <geojickson@gmail.com>
2026-06-21 23:10:41 +0530

    a

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
 
 client.on('qr', (qr) => {
     console.log('Scan this QR code:');
